9dc7002eabcd1695971e9be5e198264843343b7b
SebastianLealTrejo <sebastian.leal02@utrgv.edu>
2024-10-22 13:08:36 -0500

    some minor changes made to the html file

---
+++ WelcomePage/welcome.js
@@ -0,0 +1,4 @@
+// In this java script file we will create the toggles that will connect our hyper links with their proper destinations
+
+
+// 1 Make it so that once log in href is clicked the proper log in form will show up
